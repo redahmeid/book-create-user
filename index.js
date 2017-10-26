7588dd5be9559b0736a0986e8b8a7b5a4181fe09
redahmeid <reda@redahmeid.com>
2017-10-26 08:23:34 +0100

    add into neo4j

--- index.js
+++ index.js
@@ -2,13 +2,34 @@
 
 const awsServerlessExpress = require('aws-serverless-express')
 const app = require('./app')
-const server = awsServerlessExpress.createServer(app)
+var neo4j = require('neo4j-driver').v1;
+var driver = neo4j.driver("bolt://hobby-djjfigaajbbfgbkeaecpfepl.dbs.graphenedb.com:24786", neo4j.auth.basic("openlight", "b.6GGBQb5zVyyC.AGcbmyfCH0dLlifb"));
 
 exports.handler = function(event, context,callback) {
 
     var body = JSON.parse(event.body);
     console.log(body);
 
-    callback(null, { statusCode: 200, body: JSON.stringify(body) });
+    var session = driver.session();
+  
+    var name = body.name;
+    var email = body.email;
+    var response;
+    session
+        .run("CREATE (n:User {name:'"+name+"', email:'"+email+"'}) RETURN n.name")
+        .then(function(result) {
+            result.records.forEach(function(record) {
+                response = record;
+                console.log(record)
+            });
+  
+            session.close();
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+  
+
+    callback(null, { statusCode: 201, body: JSON.stringify(response) });
 
 }
