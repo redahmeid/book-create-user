a24aaf720dd66f19cc8430d3ebfa3f3ffc0b26c3
redahmeid <reda@redahmeid.com>
2017-10-26 10:40:27 +0100

    some debug to workout why it's timing out

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = require('./app')
 var neo4j = require('neo4j-driver').v1;
 var driver = neo4j.driver("bolt://hobby-djjfigaajbbfgbkeaecpfepl.dbs.graphenedb.com:24786", neo4j.auth.basic("openlight", "b.6GGBQb5zVyyC.AGcbmyfCH0dLlifb"));
 
-exports.handler = function(event, context,callback) {
+exports.handler = function(event, context, callback) {
 
   var body = JSON.parse(event.body);
     console.log(body);
@@ -15,19 +15,22 @@ exports.handler = function(event, context,callback) {
     var email = body.email;
     var response;
     session
-        .run("CREATE (n:User {name:'"+name+"', email:'"+email+"'}) RETURN n")
+        .run("CREATE (n:User {name:'"+name+"', email:'"+email+"'}) RETURN n.id")
         .then(function(result){
             result.records.forEach(function(record) {
-                response = record;
-                console.log(record) 
+                response = record.get("id");
+                console.log(record.get("id")) 
             });
             console.log("finished")
-            session.close();
+            
             callback(null, { statusCode: 201, body: JSON.stringify(response) });
+            session.close();
+            
         })
         .catch(function(error) {
             callback(null, { statusCode: 500, body: {"error":"error"} });
         });
 
         console.log("here 1")
+        
 }
